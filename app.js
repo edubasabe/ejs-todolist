a212911c296682a5bc6961176fa4f83f4488d648
David Basabe <david.basabe@skyairline.com>
2020-06-28 19:18:58 -0400

    :sparkles: My first ejs template

--- app.js
+++ app.js
@@ -2,14 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+
   const today = new Date();
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("<h1>Yay! It's the weeekend!</h1>");
-  } else {
-    res.send("<h1>Boo I have to work</h1>");
+  const currentDay = today.getDay();
+  let day;
+
+  switch (currentDay) {
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+      day = 'Sunday';
+      break;
   }
+
+  res.render('list', { kindOfDay: day });
 });
 
 app.listen(8080, () => {
